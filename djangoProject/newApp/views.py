c264423d9242a18295a4bf591275ccfd207ceef7
mmali02 <mmali@westmont.edu>
2024-01-31 20:33:23 -0800

    creating a view for the index html

--- djangoProject/newApp/views.py
+++ djangoProject/newApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+# Defining a view for inside index.html, (turning a request into a response)
+def index(request):
+    render(request, "newApp/index.html", {})
