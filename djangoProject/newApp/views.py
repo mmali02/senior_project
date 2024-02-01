7937a818f2a6d7317713fbef86bae1ae727f90f5
mmali02 <mmali@westmont.edu>
2024-01-31 20:45:44 -0800

    made a mistake with not returning the request for the html file

--- djangoProject/newApp/views.py
+++ djangoProject/newApp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 # Defining a view for inside index.html, (turning a request into a response)
 def index(request):
-    render(request, "newApp/index.html", {})
+    return render(request, "newApp/index.html", {})
