e56b5c9d4fb38956ff3eb4b030eb49e4858fe377
mmali02 <mmali@westmont.edu>
2024-02-28 10:28:09 -0800

    starting from scratch to see if it will work

--- djangoProject/newApp/admin.py
+++ djangoProject/newApp/admin.py
@@ -1,8 +1,2 @@
 from django.contrib import admin
-from .models import Vegetable
-from .models import Fruit
-from .models import Plant
 
-admin.site.register(Vegetable)
-admin.site.register(Fruit)
-admin.site.register(Plant)
