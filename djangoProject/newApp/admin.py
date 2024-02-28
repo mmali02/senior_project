8fd8c09ad865b4322c1f76e6037bdcb88a61602b
mmali02 <mmali@westmont.edu>
2024-02-28 10:45:10 -0800

    adding in the models

--- djangoProject/newApp/admin.py
+++ djangoProject/newApp/admin.py
@@ -1,2 +1,10 @@
 from django.contrib import admin
+from .models import Fruit
+from .models import Vegetable
+from .models import Plant
+
+
+admin.site.register(Fruit)
+admin.site.register(Vegetable)
+admin.site.register(Plant)
 
