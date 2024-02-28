9cadab4efc12c297e3ee1eb1a67eb2c3b29b53ad
mmali02 <mmali@westmont.edu>
2024-02-27 23:54:32 -0800

    adding the models for fruits, veggies, and plants and trying to connect it to the admin page

--- djangoProject/newApp/admin.py
+++ djangoProject/newApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Vegetables
+from .models import Fruits
+from .models import Plants
 
-# Register your models here.
+admin.site.register(Vegetables)
+admin.site.register(Fruits)
+admin.site.register(Plants)
--- djangoProject/newApp/models.py
+++ djangoProject/newApp/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
-class Plant(models.Model):
+class Vegetables(models.Model):
+    name = models.CharField('Vegetable name', max_length=200)
+    latin_name = models.CharField('Latin name', max_length=200)
+    season = models.CharField(max_length=120)
+    water = models.CharField(max_length=120)
+    soil = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Fruits(models.Model):
+    name = models.CharField('Fruit name', max_length=200)
+    latin_name = models.CharField('Latin name', max_length=200)
+    season = models.CharField(max_length=120)
+    water = models.CharField(max_length=120)
+    soil = models.CharField(max_length=120)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Plants(models.Model):
     name = models.CharField('Plant name', max_length=200)
     latin_name = models.CharField('Latin name', max_length=200)
     season = models.CharField(max_length=120)
