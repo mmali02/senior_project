133a0dbb2ac67d27e1351ec5af5f1ca1d0d7ad8d
John Rodkey <rodkey@westmont.edu>
2024-04-11 11:08:04 -0700

    WIP: add views to allow querying in forms

---
+++ djangoProject/newApp/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import ChoiceField
+
+from .models import Plant, Fruit, Vegetable
+
+class SearchPlantForm(forms.Form):
+    query = forms.CharField(required=False)
+    soilfield = ChoiceField(
+        choices=[
+            ("", "Soil Type"),
+            ("Clay soil", "Clay soil"),
+            ("Loam soil", "Loam soil"),
+            ("Sandy soil", "Sandy soil"),
+        ],
+        label="",
+        required=False,
+    )
--- djangoProject/newApp/views.py
+++ djangoProject/newApp/views.py
@@ -22,3 +22,83 @@ def plant_info_base(request):
     veg_list = Vegetable.objects.all()
     plant_list = Plant.objects.all()
     return render(request, 'newApp/plant_info_base.html', {'fruit_list': fruit_list, 'veg_list': veg_list, 'plant_list': plant_list})
+
+
+def search(request):
+    search_form = SearchPlantForm(request.GET)
+    if search_form.is_valid():
+        data = search_form.cleaned_data
+        query = data["query"]
+        soilfield = data["soilfield"]
+        sunfield = data["sunfield"]
+
+        plants = search_plants( query, soilfield=soilfield , sunfield=sunfield)
+        fruits = search_fruits( query, soilfield=soilfield , sunfield=sunfield)
+        vegetables = search_vegetables( query, soilfield=soilfield , sunfield=sunfield)
+
+        return render(
+            request,
+            "garden/index.html",
+            {
+                "query": query,
+                "plants": plants,
+                "fruits": fruits,
+                "vegetables": vegetables
+            },
+        )
+    else:
+        return HttpResponseBadRequest()
+
+def search_plants(
+    query: str,
+    soilfield: Optional[str] = None,
+    sunfield: Optional[str] = None,
+) -> List[Plant]:
+
+    if sortfield == "entered_at":
+        plants = Plant.objects.all().order_by(
+            sortorder + sortfield
+        ) 
+    else:
+        plants = Plant.objects.all()
+
+    results = plants.filter(soilfield=Plant.soil).filter(sunfield=Plant.sun)
+
+    return results
+
+
+def search_fruit(
+    query: str,
+    soilfield: Optional[str] = None,
+    sunfield: Optional[str] = None,
+) -> List[Fruit]:
+
+    if sortfield == "entered_at":
+        fruit = Fruit.objects.all().order_by(
+            sortorder + sortfield
+        ) 
+    else:
+        fruit = Fruit.objects.all()
+
+    results = fruit.filter(soilfield=Fruit.soil).filter(sunfield=Fruit.sun)
+
+    return results
+
+
+def search_vegetables(
+    query: str,
+    soilfield: Optional[str] = None,
+    sunfield: Optional[str] = None,
+) -> List[Vegetable]:
+
+    if sortfield == "entered_at":
+        vegetables = Vegetable.objects.all().order_by(
+            sortorder + sortfield
+        ) 
+    else:
+        vegetables = Vegetable.objects.all()
+
+    results = vegetables.filter(soilfield=Vegetable.soil).filter(sunfield=Vegetable.sun)
+
+    return results
+
